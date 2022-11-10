5af302f46ec042ffbc3af6239c43038a39c1d0e4
Maryam Yousefvand <yousefvand.mrym@gmail.com>
2022-11-11 03:26:42 +0330

    add-user-list-page

---
+++ pages/dashboard/user-list.js
@@ -0,0 +1,16 @@
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import UserList from '@components/UserList'
+
+
+
+const Dashboard = () => {
+    const router = useRouter()
+    const [isLogin, setIsLogin] = useState()
+    return (
+        <UserList />
+
+    )
+}
+export default Dashboard

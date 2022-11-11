527da7909f327078cbf99839fa1845a1061e57f0
Maryam Yousefvand <yousefvand.mrym@gmail.com>
2022-11-11 04:11:32 +0330

    add-json-server-url

--- features/api/index.js
+++ features/api/index.js
@@ -2,7 +2,8 @@ import { QueryClient } from "react-query";
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: ""
+    // baseURL: "https://api.exgain.ir/wallet/api/v1"
+    baseURL: 'http://localhost:3001'
 })
 
 export const queryClient = new QueryClient()
